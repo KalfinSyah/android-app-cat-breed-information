46eea26e6fa761a4b9019910ff9129487370d76b
KalfinSyah <kalfinsyah.km@gmail.com>
2025-01-25 23:38:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/catbreedinformation/data/CatBreed.kt
@@ -0,0 +1,14 @@
+package com.example.catbreedinformation.data
+
+data class CatBreed(
+    val name: String,
+    val imageUrl: String,
+    val origin: String,
+    val lifespan: String,
+    val appearance: String,
+    val description: String,
+)
+
+//fun getCatBreeds(): List<CatBreed> {
+//
+//}
